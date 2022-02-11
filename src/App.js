96efc6923e481657ba3d624ee484e252655aaff1
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-02-10 22:15:55 -0300

    Remover um repositório da sua API

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const newRepositories = repositories.filter((repository) => repository.id !== id);
+    setRepositories(newRepositories);
   }
 
   return (
