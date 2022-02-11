844ba4756c47862c76986af39604f7620c62a8aa
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-02-10 22:14:09 -0300

    Adicionar um repositório a sua API

--- src/App.js
+++ src/App.js
@@ -13,7 +13,15 @@ function App() {
   }, []);
 
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
